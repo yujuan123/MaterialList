cff9d9238ade1f14fa557603fe7cdf6a336af316
yujuan <15809216911@163.com>
2016-09-02 15:45:53 +0800

    Build a reducer to return state

--- public/src/reducers/result.js
+++ public/src/reducers/result.js
@@ -1,3 +1,9 @@
-/**
- * Created by yujuan on 16-9-2.
- */
+//reducers 里面有 好多数据 state
+const result = (state=[],action)=>{
+  switch (action.type){
+    case 'MATERIAL_LOADED':
+      return action.data;
+    return state;
+  }
+};
+export default result;
