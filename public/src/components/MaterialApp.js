52f17010e49b13df6b433cb4d7f7e1b5a0fd89b9
yujuan <15809216911@163.com>
2016-09-07 17:54:12 +0800

    Import DeleteMaterialPackage component

--- public/src/components/MaterialApp.js
+++ public/src/components/MaterialApp.js
@@ -3,12 +3,14 @@ import React ,{Component} from 'react';
 
 //添加 被包好的组件
 import AddMaterialPackage from '../containers/AddMaterialPackage';
+import DeleteMaterialPackage from './DeleteMaterial';
 
 class MaterialApp extends Component{
   render(){
     return (
         <div>
           <AddMaterialPackage/>
+          <DeleteMaterialPackage/>
         </div>
     )
   }
